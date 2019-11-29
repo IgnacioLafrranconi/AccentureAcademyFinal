3d0e523e6bdd718ff070cae6dbdc066eb74ba72a
T11641.Training16 <T11641.Training16@accenture.com>
2019-11-29 16:51:13 -0300

    Add project files.

---
+++ AcademyFinal/Controllers/BookController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AcademyFinal.Models;
+
+namespace AcademyFinal.Controllers
+{
+    public class BookController : Controller
+    {
+        private BookStoreEntities db;
+
+        public BookController()
+        {
+            db = new BookStoreEntities();
+        }
+
+        public ActionResult Agregar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Agregar(Book book, Author authors, IEnumerable<int> genres, Publisher publisher)
+        {
+            foreach (int item in genres)
+            {
+                Genre genreReferencia = new Genre();
+                genreReferencia = db.Genre.Find(item);
+                book.Genre.Add(genreReferencia);
+            }
+
+            book.Author.Add(authors);            
+            book.Publisher.Add(publisher);
+
+            this.db.Book.Add(book);
+            this.db.SaveChanges();
+
+            return Content("Libro Agregado");
+        }
+
+        public ActionResult Listar()
+        {
+            List<Book> listBook = new List<Book>();
+            return View();
+        }
+
+    }
+}
---
+++ AcademyFinal/Data/Script.sql
@@ -0,0 +1,88 @@
+﻿CREATE DATABASE BookStore;
+GO
+
+USE BookStore;
+GO
+
+
+Create Table Book
+(
+	Id int primary key identity(1,1),
+	Title varchar(100) not null,
+);
+GO
+
+Create table Author 
+(
+	Id int primary key identity(1,1),
+	[Name] varchar(50) not null,
+);
+GO
+
+Create table Publisher 
+(
+	Id int primary key identity(1,1),
+	[Name] varchar(50) not null,
+);
+GO
+
+Create table Genre 
+(
+	Id int primary key identity(1,1),
+	[Type] varchar(50) not null,
+);
+GO
+
+Create Table WritenBy
+(	
+	Id int primary key identity(1,1),
+	Author_Id int,
+	Book_Id int,
+	CONSTRAINT FK_AUTOR_BOOK FOREIGN KEY (Author_Id) 
+		REFERENCES Author(Id) 
+		ON DELETE CASCADE,
+	CONSTRAINT FK_BOOK_AUTHOR FOREIGN KEY (Book_Id) 
+		REFERENCES Book(Id) 
+		ON DELETE CASCADE,
+	CONSTRAINT UQ_BOOK_AUTHOR UNIQUE (Author_Id, Book_Id)
+);
+GO
+
+
+Create Table PublishedBy
+(	
+	Id int primary key identity(1,1),
+	Publisher_Id int not null,
+	Book_Id int not null,
+	CONSTRAINT FK_AUTOR_PUBLISHER FOREIGN KEY (Publisher_Id) 
+		REFERENCES Publisher(Id) 
+		ON DELETE CASCADE,
+	CONSTRAINT FK_BOOK_PUBLISHER FOREIGN KEY (Book_Id) 
+		REFERENCES Book(Id) 
+		ON DELETE CASCADE,
+	CONSTRAINT UQ_BOOK_PUBLISHER UNIQUE (Publisher_Id, Book_Id)
+);
+GO
+
+Create Table GenreIs
+(	
+	Id int primary key identity(1,1),
+	Genre_Id int not null,
+	Book_Id int not null,
+	CONSTRAINT FK_AUTOR_GENRE FOREIGN KEY (Genre_Id) 
+		REFERENCES Genre(Id) 
+		ON DELETE CASCADE,
+	CONSTRAINT FK_BOOK_GENRE  FOREIGN KEY (Book_Id) 
+		REFERENCES Book(Id) 
+		ON DELETE CASCADE,
+	CONSTRAINT UQ_BOOK_GENRE UNIQUE (Genre_Id, Book_Id)
+);
+GO
+
+
+select Title,[Name] 
+from Book
+join WritenBy
+on WritenBy.Book_Id = Book.Id
+join Author
+on WritenBy.Author_Id = Author.Id
